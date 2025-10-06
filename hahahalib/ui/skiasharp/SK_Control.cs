8c1a83a7c69c6c40046b2aadbff70aac1c7e4279
hahaha <hahaha0417@hotmail.com>
2025-10-06 18:28:32 +0800

    fix

---
+++ hahahalib/ui/skiasharp/SK_Control.cs
@@ -0,0 +1,22 @@
+﻿
+using SkiaSharp.Views.Desktop;
+
+namespace hahahalib
+{
+    namespace ui
+    {
+
+
+        public class SK_Control : SKControl
+        {
+        
+
+            public SK_Control()
+            {
+                
+            }
+
+        }
+
+    }
+}
---
+++ hahahalib/ui/tab_controll/Tab_Controll_Base.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace hahahalib
+{
+    namespace ui
+    {
+        public class Tab_Controll_Base : TabControl
+        {
+            private bool _hideTabHeader = false;
+
+            /// <summary>
+            /// 設定是否隱藏 Tab 頁籤
+            /// </summary>
+            [Category("Appearance")]
+            [Description("設定是否隱藏 TabControl 的頁籤")]
+            [DefaultValue(false)]
+            public bool HideTabHeader
+            {
+                get => _hideTabHeader;
+                set
+                {
+                    if (_hideTabHeader != value)
+                    {
+                        _hideTabHeader = value;
+                        this.Invalidate(); // 重新繪製
+                    }
+                }
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                const int TCM_ADJUSTRECT = 0x1328;
+                if (m.Msg == TCM_ADJUSTRECT && !DesignMode && _hideTabHeader)
+                {
+                    m.Result = (IntPtr)1; // 隱藏 tab header
+                    return;
+                }
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
